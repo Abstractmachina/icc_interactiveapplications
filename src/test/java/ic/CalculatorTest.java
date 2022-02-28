f487e24395e2508ab8b2c9b3b58490a2558ab73d
tc1421 <tc1421@ic.ac.uk>
2022-02-28 23:12:28 +0000

    addingSecondValueStoresInCorrectOrder() added. test failed.

--- src/test/java/ic/CalculatorTest.java
+++ src/test/java/ic/CalculatorTest.java
@@ -16,5 +16,14 @@ public class CalculatorTest {
 
     assertThat(calc.getVal1(), is(1));
   }
+  @Test
+  public void addingSecondValueStoresInCorrectOrder() {
+    Calculator calc = new Calculator();
+   // calc.addValue(1);
+    //calc.addValue(2);
+
+    assertThat(calc.getVal1(), is(1));
+    //assertThat(calc.getVal2(), is(2));
+  }
 
 }
