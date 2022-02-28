b18b25da1a93c7463db95ade14c9312b46b145b5
tc1421 <tc1421@ic.ac.uk>
2022-02-28 22:21:24 +0000

    logic functionality implemented.

--- src/main/java/ic/doc/ReversePolishCalculatorApp.java
+++ src/main/java/ic/doc/ReversePolishCalculatorApp.java
@@ -2,19 +2,66 @@ package ic.doc;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SingleSelectionModel;
+import javax.swing.WindowConstants;
 
 public class ReversePolishCalculatorApp {
 
+  private int val1 = 0;
+  private int val2 = 0;
+  private String displayTxt = "";
+
+  private JTextField textField;
+
   public static void main(String[] args) {
     new ReversePolishCalculatorApp().display();
+  }
+
+  private void addValue(int v) {
+    String vs = Integer.toString(v);
+    if (val1 == 0) {
+      val1 = v;
+      displayTxt += vs;
+      displayTxt += " ";
+    }
+    else {
+      if (val2 == 0) {
+        val2 = v;
+        displayTxt += vs;
+        displayTxt += " ";
+      }
+      else {
+        val1 = val2;
+        val2 = v;
+        displayTxt = String.format("%d %d ", val1, val2);
+      }
+    }
+  }
+
+  private void addToDisplay(int value) {
+    displayTxt += Integer.toString(value);
+    displayTxt += " ";
+  }
+
+  private void reset() {
+    val1 = 0;
+    val2 = 0;
+    displayTxt = "";
+  }
+
+  private void clear() {
+    reset();
+    textField.setText("");
   }
 
   private void display() {
 
     JFrame frame = new JFrame("Reverse Polish Calculator");
     frame.setSize(400, 300);
-
 
     JPanel panel = new JPanel();
 
@@ -24,50 +71,75 @@ public class ReversePolishCalculatorApp {
     JButton b_four = new JButton("4");
     JButton b_plus = new JButton("+");
     JButton b_minus = new JButton("-");
+    JButton b_clear = new JButton("clear");
 
-    JTextField textField = new JTextField(10);
 
+    textField = new JTextField(10);
 
-    b_one.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
+    b_one.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            addValue(1);
+            textField.setText(displayTxt);
+          }
+        });
 
-      }
-    });
+    b_two.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            addValue(2);
+            textField.setText(displayTxt);
+          }
+        });
 
-    b_two.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
+    b_three.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            addValue(3);
+            textField.setText(displayTxt);
+          }
+        });
 
-      }
-    });
+    b_four.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            addValue(4);
+            textField.setText(displayTxt);
+          }
+        });
 
-    b_three.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
+    b_plus.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            int sum = val1+val2;
+            displayTxt = Integer.toString(sum);
+            textField.setText(displayTxt);
+            reset();
+          }
+        });
+    b_minus.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            int diff = val1-val2;
+            displayTxt = Integer.toString(diff);
+            textField.setText(displayTxt);
+            reset();
+          }
+        });
 
-      }
-    });
-
-    b_four.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-
-      }
-    });
-
-    b_plus.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-
-      }
-    });
-    b_minus.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        textField.setText("tada");
-      }
-    });
+    b_clear.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            clear();
+          }
+        });
 
     panel.add(b_one);
     panel.add(b_two);
@@ -75,6 +147,7 @@ public class ReversePolishCalculatorApp {
     panel.add(b_four);
     panel.add(b_plus);
     panel.add(b_minus);
+    panel.add(b_clear);
 
     panel.add(textField);
 
